71aaf440a31b84cad5072a773d2efe41d3324955
Carlos Melendez <homex_1@hotmail.com>
2015-06-17 09:11:35 -0500

    Prueba 3,4,5,6 - kata potter

--- ruby/potter/1/Potter.rb
+++ ruby/potter/1/Potter.rb
@@ -1,9 +1,11 @@
 class Potter
 
+  def book
+  	["Libro 1", "Libro 2"]
+  end
 
  def price(books)
    books.size == 0 ? 0 : 8
  end
-
 
 end
--- ruby/potter/1/test.rb
+++ ruby/potter/1/test.rb
@@ -10,7 +10,10 @@ class PotterTest < Minitest::Test
   def test_price
   	assert_equal(0, @potter.price([]))
 		assert_equal(8, @potter.price([0]))
-
+		assert_equal(8, @potter.price([1]))
+		assert_equal(8, @potter.price([2]))
+		assert_equal(8, @potter.price([3]))
+		assert_equal(8, @potter.price([4]))
   end
 
   
